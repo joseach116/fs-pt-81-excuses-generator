038b663ec774a33ec77993028bccba9e2ededa7e
joseach116 <joseluisamadochacon@gmail.com>
2024-10-20 09:03:41 +0000

    excuses-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
+// Puedes revisar mas estricto
+
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const excuse = document.querySelector("#excuse");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excuse.innerHTML = `${getValue(who)} ${getValue(action)} ${getValue(
+    what
+  )} ${getValue(when)}`;
 };
+
+const who = ["A dog", "A cat", "A horse", "My neighbour", "My boss"];
+const action = ["ate", "bit", "burnt", "took", "threw"];
+const what = ["my leg", "my book", "my car", "my keys", "my kids", "my money"];
+const when = [
+  "during my studies",
+  "while I was eating",
+  "when driving a car",
+  "while having a beer"
+];
+
+const getRandom = array => Math.floor(Math.random() * array.length);
+const getValue = array => array[getRandom(array)];
+
+console.log(getRandom(who));
+console.log(getRandom(action));
+console.log(getRandom(what));
+console.log(getRandom(when));
